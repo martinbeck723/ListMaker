15fe367fc84361914e794a94ee270f3fe825aeff
martinbeck723 <xishuoliu514@gmail.com>
2020-05-18 12:30:30 -0400

    message

---
+++ app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.raywenderlich.listmaker
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.text.InputType
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listsRecyclerView: RecyclerView
+    val listDataManager: ListDataManager = ListDataManager(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            showCreateListDialog()
+        }
+        // 1
+        val lists = listDataManager.readLists()
+        listsRecyclerView = findViewById<RecyclerView>(R.id.lists_recyclerview)
+// 2
+        listsRecyclerView.layoutManager = LinearLayoutManager(this)
+// 3
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showCreateListDialog() {
+// 1
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+// 2
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+// 3
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            val list = TaskList(listTitleEditText.text.toString())
+            listDataManager.saveList(list)
+            val recyclerAdapter = listsRecyclerView.adapter as
+                    ListSelectionRecyclerViewAdapter
+            recyclerAdapter.addList(list)
+            dialog.dismiss()
+        }
+// 4
+        builder.create().show()
+    }
+
+    private fun showListDetail(list: TaskList) {
+// 1
+        val listDetailIntent = Intent(this, ListDetailActivity::class.java)
+// 2
+        listDetailIntent.putExtra(INTENT_LIST_KEY, list)
+// 3
+        startActivity(listDetailIntent)
+    }
+}
